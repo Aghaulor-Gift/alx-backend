d474e6ea6fcd6a3a0dcc4b9a29b5ec9a5a05087d
Gift <aghaulor.gift@gmail.com>
2024-09-24 11:56:56 +0000

    Job creation function

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,27 @@
+#!/usr/bin/yarn dev
+import kue from 'kue';
+
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '4153518780',
+  message: 'This is the notification message',
+};
+
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (err) {
+      console.error('Notification job failed to create');
+      return;
+    }
+    console.log(`Notification job created: ${job.id}`);
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
